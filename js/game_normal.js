a9f86b1920acbe357f53b9d4b7e3ab3758597509
Grace <lunay0325@gmail.com>
2018-12-11 23:40:11 +0800

    Complete the function that a mole can randomly show and disapprea

--- js/game_normal.js
+++ js/game_normal.js
@@ -1,36 +1,53 @@
-window.onload = function () {
+window.onload = function() {
+  const holes = document.querySelectorAll('.hole');
+  const scoreBoard = document.querySelector('.score');
+  const moles = document.querySelectorAll('.mole');
+  const startBtn = document.getElementById('start_btn');
+  let titleH1 = document.getElementById('title');
+  let lastHole, clearTimeout;
+  let timeUp = false;
+  let score = 0;
+  let gameTime = 10000;
+  startBtn.addEventListener('click', function() {
+    showBtnAnimation();
+    startGame();
+  }, false);
 
-    const holes = document.querySelectorAll('.hole');
-    const scoreBoard = document.querySelector('.score');
-    const moles = document.querySelectorAll('.mole');
-    const startBtn = document.getElementById('start_btn');
-    let titleH1 = document.getElementById('title');
+  function showBtnAnimation() {
+    event.preventDefault();
+    startBtn.classList.add('animate');
+    // 按钮动画延时，按钮动画结束后发生的事：换为正常状态（class中的animate去掉），开始按钮消失
+    setTimeout(() => {
+      startBtn.classList.remove('animate');
+      startBtn.style.display = 'none';
+    }, 700);
+  }
 
-    let lastHole;
-    let timeUp = false;
-    let score = 0;
-    let gameTime = 10000;
+  function startGame() {
+    // TODO: 写开始新游戏后发生的事
+  }
 
+  function genRandomNum() {
+    return Math.round(Math.random() * 5 + 1);
+  }
 
-    startBtn.addEventListener('click', function () {
-        showBtnAnimation();
-        startGame();
-    }, false);
+  function genRandomTime() {
+    return Math.round(Math.random() * 1000 + 400);
+  }
 
-    function showBtnAnimation() {
-        event.preventDefault();
+  function showMole() {
+    lastHole = `.hole${genRandomNum()}`;
+    $(lastHole).toggleClass("up");
+  }
 
-        startBtn.classList.add('animate');
-        // 按钮动画延时，按钮动画结束后发生的事：换为正常状态（class中的animate去掉），开始按钮消失
-        setTimeout(() => {
-            startBtn.classList.remove('animate');
-            startBtn.style.display = 'none';
-        }, 700);
-    }
+  function disappearMole() {
+    $(lastHole).toggleClass("up");
+  }
 
-
-    function startGame() {
-        // TODO: 写开始新游戏后发生的事
-    }
-
+  function genMoleRandomly() {
+    showMole();
+    clearTimeout = setTimeout(function() {
+      disappearMole();
+    }, genRandomTime());
+  }
 };
