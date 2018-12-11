15295647b2a17d6f400b783033676baec6d317c3
Grace <lunay0325@gmail.com>
2018-12-12 01:00:59 +0800

    Complete the function that can add scores to the board when hitting on the mole

--- js/game_normal.js
+++ js/game_normal.js
@@ -1,11 +1,11 @@
 window.onload = function() {
   const holes = document.querySelectorAll('.hole');
-  const scoreBoard = document.getElementById('.score');
+  const scoreBoard = document.getElementById('score');
   const moles = document.querySelectorAll('.mole');
   const startBtn = document.getElementById('start_btn');
   const game = document.getElementById('game');
   let titleH1 = document.getElementById('title');
-  let lastHole, clearTimeout;
+  let lastHole, clearMole, chosenHole;
   let timeUp = false;
   let score = 0;
   let gameTime = 10000;
@@ -26,6 +26,8 @@ window.onload = function() {
 
   function startGame() {
     // TODO: 写开始新游戏后发生的事
+    genMoleRandomly();
+    hitMole();
   }
 
   function genRandomNum() {
@@ -38,18 +40,35 @@ window.onload = function() {
 
   function showMole() {
     lastHole = `.hole${genRandomNum()}`;
-    $(lastHole).toggleClass('up');
+    $(lastHole).toggleClass('up', true);
   }
 
   function disappearMole() {
-    $(lastHole).toggleClass('up');
+    $(lastHole).toggleClass('up', false);
   }
 
   function genMoleRandomly() {
     showMole();
-    clearTimeout = setTimeout(function() {
+    clearMole = setTimeout(function() {
       disappearMole();
       genMoleRandomly();
     }, genRandomTime());
+  }
+
+  function addScores() {
+    scoreBoard.innerText = parseInt(scoreBoard.innerText) + 1;
+  }
+
+  function hitMole() {
+    game.addEventListener('click', function(event) {
+      chosenHole = event.target.parentNode.getAttribute('class').split(' ')[1];
+      if (chosenHole === lastHole.slice(1)) {
+        clearTimeout(clearMole);
+        disappearMole();
+        addScores();
+        setTimeout(function(){
+          genMoleRandomly();
+      },400)}
+    })
   }
 };
